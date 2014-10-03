a3c37fa3c412bdbe0fedfb3f333d5a0dcffabee4
Michael Lam <lamm@eecs.oregonstate.edu>
2014-10-02 20:13:56 -0700

    Debug: init more below from root

--- src/HCSearchLib/SuccessorFunction.cpp
+++ src/HCSearchLib/SuccessorFunction.cpp
@@ -1795,7 +1795,7 @@ namespace HCSearch
 			ImgLabeling* YTruth, ILossFunction* lossFunc, int timeStep, int timeBound)
 	{
 		const double BEST_LOSS_INIT_VALUE = 100000;
-		const int BST_INITIALIZATION_LEVEL = 2;
+		const int BST_INITIALIZATION_LEVEL = 4;
 
 		if (YTruth == NULL)
 		{
