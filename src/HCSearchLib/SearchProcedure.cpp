2ebae487f0c8e147052f948596d67241532f5f18
Michael Lam <lamm@eecs.oregonstate.edu>
2014-10-30 19:06:35 -0700

    More freeing memory (needs to be verified)

--- src/HCSearchLib/SearchProcedure.cpp
+++ src/HCSearchLib/SearchProcedure.cpp
@@ -870,6 +870,17 @@ namespace HCSearch
 				vector< double > worstLosses;
 				sortNodes(searchType, candidateSet, bestFeatures, bestLosses, worstFeatures, worstLosses);
 				trainRanker(heuristicModel, bestFeatures, bestLosses, worstFeatures, worstLosses);
+
+				// clean up
+				if (!Global::settings->CHECK_FOR_DUPLICATES)
+				{
+					for (vector< SearchNode* >::iterator it = candidateSet.begin(); it != candidateSet.end(); it++)
+					{
+						SearchNode* state = *it;
+						if (state != bestHeuristicNode && state != bestCostNode)
+							delete state;
+					}
+				}
 			}
 
 			/***** done with this search step *****/
@@ -948,8 +959,13 @@ namespace HCSearch
 			//	ImgLabeling YPred = state->getY();
 			//	candidateLosses.push_back(searchSpace->computeLoss(YPred, *YTruth));
 			//}
-			delete state;
+			if (state != bestHeuristicNode && state != bestCostNode)
+				delete state;
 		}
+		if (bestHeuristicNode != NULL)
+			delete bestHeuristicNode;
+		if (bestCostNode != NULL)
+			delete bestCostNode;
 		//if (YTruth != NULL)
 		//{
 		//	stringstream ssLosses;
@@ -983,6 +999,10 @@ namespace HCSearch
 			expansionSet = bestHeuristicNode->generateSuccessorNodes(true, timeStep, timeBound, YTruth);
 
 		// reset best heuristic
+		if (bestHeuristicNode != bestCostNode)
+		{
+			delete bestHeuristicNode;
+		}
 		bestHeuristicNode = NULL;
 
 		LOG() << "num expansion=" << expansionSet.size() << endl;
@@ -1012,6 +1032,11 @@ namespace HCSearch
 				// get the best heuristic node
 				if (bestHeuristicNode == NULL || state->getHeuristic() < bestHeuristicNode->getHeuristic())
 				{
+					if (bestHeuristicNode != NULL && bestHeuristicNode != bestCostNode)
+					{
+						delete bestHeuristicNode;
+						bestHeuristicNode = NULL;
+					}
 					bestHeuristicNode = state;
 					discard = false;
 				}
@@ -1019,6 +1044,12 @@ namespace HCSearch
 				// get the best cost node
 				if (state->getCost() < bestCostNode->getCost())
 				{
+					if (bestCostNode != NULL && bestCostNode != bestHeuristicNode)
+					{
+						delete bestCostNode;
+						bestCostNode = NULL;
+					}
+
 					bestCostNode = state;
 					discard = false;
 				}
